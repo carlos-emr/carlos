8eaa46373e3a73e1df7e3876b14535576c51dd01
Peter Hutten-Czapski <phc@srpc.ca>
2026-05-12 11:16:43 -0400

    Fix argument handling in createdatabase_on.sh
    Signed-off-by: Peter Hutten-Czapski <phc@srpc.ca>

--- database/mysql/createdatabase_on.sh
+++ database/mysql/createdatabase_on.sh
@@ -5,4 +5,8 @@ if [ $# -ne 3 ] && [ $# -ne 4 ]; then
   echo "or [database user] [database password] [database name] supressPwdGen"
 	exit
 fi
+if [ $# -ne 3 ]; then
+ 	./createdatabase_generic.sh $1 $2 $3 on 9 $4
+	exit
+fi
 ./createdatabase_generic.sh $@ on 9
