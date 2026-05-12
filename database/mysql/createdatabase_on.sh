1523318cf7bdcb6c1a2bba7e1656d61ad262bf87
Peter Hutten-Czapski <phc@srpc.ca>
2026-05-12 11:11:14 -0400

    Enhance usage message for createdatabase_on.sh
    Updated usage message to include optional parameter for suppressing password generation.
    
    Signed-off-by: Peter Hutten-Czapski <phc@srpc.ca>

--- database/mysql/createdatabase_on.sh
+++ database/mysql/createdatabase_on.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # required parameters : $1=user $2=password $3=databasename
-if [ "$#" -ne 3 ]; then
-	echo "Usage: ./createdatabase_on.sh [database user] [database password] [database name]"
+if [ $# -ne 3 ] && [ $# -ne 4 ]; then
+ 	echo "Usage: ./createdatabase_on.sh [database user] [database password] [database name]"
+  echo "or [database user] [database password] [database name] supressPwdGen"
 	exit
 fi
 ./createdatabase_generic.sh $@ on 9
