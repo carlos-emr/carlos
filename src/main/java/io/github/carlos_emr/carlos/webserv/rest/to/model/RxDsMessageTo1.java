cb2f66afeca177f7cd91a05019faa88652e35d36
Ben Heerema <ben@maplecreekmedical.ca>
2026-08-24 10:32:54 -0400

    fix: remove stale RxDsMessageTo1 import
    Signed-off-by: Ben Heerema <ben@maplecreekmedical.ca>

--- src/main/java/io/github/carlos_emr/carlos/webserv/rest/to/model/RxDsMessageTo1.java
+++ src/main/java/io/github/carlos_emr/carlos/webserv/rest/to/model/RxDsMessageTo1.java
@@ -37,7 +37,6 @@ import java.util.ResourceBundle;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import io.github.carlos_emr.carlos.utility.MiscUtils;
 
 
 @XmlRootElement
