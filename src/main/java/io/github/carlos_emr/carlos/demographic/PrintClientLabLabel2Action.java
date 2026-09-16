1fe65dc509a06c4e8281bbf1e0e796bf42bdd276
Michael Yingbull <michael@maplecreekmedical.ca>
2026-09-16 20:32:43 +0000

    fix(printing): remove obsolete import flagged by CI
    Signed-off-by: Michael Yingbull <michael@maplecreekmedical.ca>

--- src/main/java/io/github/carlos_emr/carlos/demographic/PrintClientLabLabel2Action.java
+++ src/main/java/io/github/carlos_emr/carlos/demographic/PrintClientLabLabel2Action.java
@@ -31,7 +31,6 @@ package io.github.carlos_emr.carlos.demographic;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
